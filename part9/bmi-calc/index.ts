059ee19f34f07b8a9ec776e579a27cf69223ab8c
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2020-10-22 14:27:55 -0400

    more part 9 exercises

--- part9/bmi-calc/index.ts
+++ part9/bmi-calc/index.ts
@@ -3,7 +3,6 @@ import { calculateBmi } from './bmi';
 const app = express();
 
 app.get('/bmi', (req, res) => {
-  console.log('req.query', req.query);
   const { height, mass } = req.query;
   if (!isNaN(Number(req.query.height)) && !isNaN(Number(req.query.mass))) {
     const bmi = calculateBmi({
@@ -14,17 +13,6 @@ app.get('/bmi', (req, res) => {
   } else {
     return res.status(400).json({ error: 'malformatted parameters' });
   }
-  /*  const { height, mass } = req.query;
-  const isValid: boolean = !isNaN(Number(height)) && !isNaN(Number(mass));
-  if (isValid) {
-    const bmi = calculateBmi({
-      height: Number(height),
-      mass: Number(mass),
-    });
-    return res.json({ height, mass, bmi });
-  } else {
-    return res.status(400).json({ error: "malformatted parameters" });
-  } */
 });
 
 const PORT = 3003;
